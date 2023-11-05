d7be464556f476d0abc82e696873a79fa6638455
mooner1022 <moonmk10221@gmail.com>
2023-11-06 00:56:18 +0900

    Add socket base code

--- src/main/kotlin/dev/hoyeon/plugins/Sockets.kt
+++ src/main/kotlin/dev/hoyeon/plugins/Sockets.kt
@@ -1,9 +1,13 @@
 package dev.hoyeon.plugins
 
+import io.ktor.network.sockets.*
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.receiveAsFlow
 import java.time.Duration
 
 fun Application.configureSockets() {
@@ -14,16 +18,52 @@ fun Application.configureSockets() {
         masking = false
     }
     routing {
-        webSocket("/ws") { // websocketSession
-            for (frame in incoming) {
-                if (frame is Frame.Text) {
-                    val text = frame.readText()
-                    outgoing.send(Frame.Text("YOU SAID: $text"))
-                    if (text.equals("bye", ignoreCase = true)) {
-                        close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
+        route("/chat") {
+            webSocket("/global") {
+                setupIncoming()
+                for (frame in incoming) {
+                    if (frame is Frame.Text) {
+                        val text = frame.readText()
+                        outgoing.send(Frame.Text("YOU SAID: $text"))
+                        if (text.equals("bye", ignoreCase = true)) {
+                            close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
+                        }
+                    }
+                }
+            }
+            webSocket("/private") {
+                setupIncoming()
+                for (frame in incoming) {
+                    if (frame is Frame.Text) {
+                        val text = frame.readText()
+                        outgoing.send(Frame.Text("YOU SAID: $text"))
+                        if (text.equals("bye", ignoreCase = true)) {
+                            close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
+                        }
                     }
                 }
             }
         }
     }
+}
+
+private suspend fun WebSocketSession.setupIncoming() {
+    incoming
+        .receiveAsFlow()
+        .buffer(Channel.UNLIMITED)
+        .collect { frame ->
+            when(frame) {
+                is Frame.Text -> {
+                    val text = frame.readText()
+                    //EventHandler.fireEventWithScope(Events.Gateway.MessageReceive(conn, text))
+                    try {
+                        //val command: GatewayCommand = jsonSerializer.decodeFromString(text)
+                        //EventHandler.fireEventWithScope(Events.Gateway.Command(conn, command))
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+                else -> {}
+            }
+        }
 }
